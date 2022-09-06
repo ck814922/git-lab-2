7d7e44ab7c250338acc2df4152018165a5ee9783
Caden Kerr <112589063+ck814922@users.noreply.github.com>
2022-09-06 18:02:29 -0400

    Create git-lab-program.cc

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Caden Kerr>
+ *        Date: <09/06/2022>
+ * Description: displays "Hello Gitt!!"
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    ut << "Hello Git!!" << endl;
+    return 0;
+}// main
